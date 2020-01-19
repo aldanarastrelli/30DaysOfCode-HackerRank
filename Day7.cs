5871691df79c5e9d1074fb2dbdf41ebfc28674c9
aldanarastrelli <54607131+aldanarastrelli@users.noreply.github.com>
2020-01-19 04:03:16 -0300

    Create Day7.cs

---
+++ Day7.cs
@@ -0,0 +1,32 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+
+
+    static void Main(string[] args) {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+        string result = "";
+        for (int i = n-1; i >= 0; i--)
+        {
+            result += arr[i];
+            if (i-1 >= 0)   result += " ";
+        }
+        Console.Out.WriteLine(result);
+    }
+}
